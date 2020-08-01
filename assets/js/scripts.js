39b6d0766349a3783a234afc3ae739cb0643d3d8
Andrés Saavedra <hola@and318.com>
2020-08-01 16:02:31 -0400

    Add tooltip

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,6 +1,12 @@
+$( document ).ready(function() {
+
 $(document).on('click', 'a[href^="#"]', function (e) {
     e.preventDefault();
     $('html, body').stop().animate({
         scrollTop: $($(this).attr('href')).offset().top -90
     }, 500, 'linear');
+});
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+})
 });
